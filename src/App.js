67cf95c03dd072b5c7f9af6fd080f792d2bdcbd6
alexmomentum <alexmomentum@gmail.com>
2024-04-20 17:46:47 -0400

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Linktree Clone</h1>
+        <p>This is the beginning of my custom Linktree-like application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
